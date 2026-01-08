1030988896db0499e2dda67463c748d26e328207
Dylan Luo <dylan.luo@m5stack.com>
2026-01-08 17:59:29 +0800

    web add fps；pin set sticks3；

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 
   Author GOB https://twitter.com/gob_52_gob (*^○^*)
 */
-#include <M5Unified.h>
 #include <esp_camera.h>
 #include <WiFi.h>
 #include <gob_GC0308.hpp>
@@ -13,26 +12,26 @@
 // --------------------------------
 // Camera GC0308
 // Pin settings
-#define CAM_PIN_PWDN    -1
-#define CAM_PIN_RESET   -1
-#define CAM_PIN_XCLK    2
-#define CAM_PIN_SIOD    12
-#define CAM_PIN_SIOC    11
-#define CAM_PIN_D7      47
-#define CAM_PIN_D6      48
-#define CAM_PIN_D5      16
-#define CAM_PIN_D4      15
-#define CAM_PIN_D3      42
-#define CAM_PIN_D2      41
-#define CAM_PIN_D1      40
-#define CAM_PIN_D0      39
-#define CAM_PIN_VSYNC   46
-#define CAM_PIN_HREF    38
-#define CAM_PIN_PCLK    45
+#define CAM_PIN_PWDN    39
+#define CAM_PIN_RESET   41
+#define CAM_PIN_XCLK    5
+#define CAM_PIN_SIOD    48
+#define CAM_PIN_SIOC    47
+#define CAM_PIN_D7      40
+#define CAM_PIN_D6      38
+#define CAM_PIN_D5      12
+#define CAM_PIN_D4      14
+#define CAM_PIN_D3      15
+#define CAM_PIN_D2      16
+#define CAM_PIN_D1      18
+#define CAM_PIN_D0      21
+#define CAM_PIN_VSYNC   42
+#define CAM_PIN_HREF    17
+#define CAM_PIN_PCLK    13
 
 extern void startCameraServer(); // app_httpd.cpp
 
-//#define USING_EXISTING_I2C
+// #define USING_EXISTING_I2C
 
 #if defined(USING_EXISTING_I2C)
 static camera_config_t camera_config =
@@ -77,28 +76,30 @@ static camera_config_t camera_config =
 #else
 static camera_config_t camera_config =
 {
-    .pin_pwdn     = -1,
-    .pin_reset    = -1,
-    .pin_xclk     = 2,
-    .pin_sscb_sda = 12,
-    .pin_sscb_scl = 11,
-    .pin_d7 = 47,
-    .pin_d6 = 48,
-    .pin_d5 = 16,
-    .pin_d4 = 15,
-    .pin_d3 = 42,
-    .pin_d2 = 41,
-    .pin_d1 = 40,
-    .pin_d0 = 39,
-    .pin_vsync = 46,
-    .pin_href  = 38,
-    .pin_pclk  = 45,
+    .pin_pwdn     = 39,
+    .pin_reset    = 41,
+    .pin_xclk     = 5,
+    .pin_sscb_sda = 48,
+    .pin_sscb_scl = 47,
+    .pin_d7 = 40,
+    .pin_d6 = 38,
+    .pin_d5 = 12,
+    .pin_d4 = 14,
+    .pin_d3 = 15,
+    .pin_d2 = 16,
+    .pin_d1 = 18,
+    .pin_d0 = 21,
+    .pin_vsync = 42,
+    .pin_href  = 17,
+    .pin_pclk  = 13,
     .xclk_freq_hz = 20000000,
     .ledc_timer   = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
     .pixel_format = PIXFORMAT_RGB565,
+    //.pixel_format = PIXFORMAT_YUV422;
     //.frame_size   = FRAMESIZE_QQVGA,
     .frame_size   = FRAMESIZE_QVGA,
+    // .frame_size   = FRAMESIZE_VGA,
     .jpeg_quality = 0,
     .fb_count     = 2,
     .fb_location  = CAMERA_FB_IN_PSRAM,
@@ -109,56 +110,46 @@ static camera_config_t camera_config =
 
 void setup()
 {
-    // M5
-    M5.begin();
-    M5.Log.setEnableColor(m5::log_target_serial, false);
 
-    M5.Display.clear(TFT_ORANGE);
 #if defined(USING_EXISTING_I2C)
     camera_config.sccb_i2c_port = M5.In_I2C.getPort();
     esp_err_t err = esp_camera_init(&ccfg);
 #else
-    M5.In_I2C.release();
     esp_err_t err = esp_camera_init(&camera_config);
 #endif
     if (err != ESP_OK)
     {
-        M5.Display.clear(TFT_BLUE);
-        M5_LOGE("Failed to init camera:%d", err);
+        // M5.Display.clear(TFT_BLUE);
+        ESP_LOGE( "setup", "Failed to init camera:%d", err);
         delay(1000 * 10);
         abort();
     }
     if(!goblib::camera::GC0308::complementDriver())
     {
-        M5_LOGE("Failed to complement GC0308");
+        ESP_LOGE("setup", "Failed to complement GC0308");
     }
 
     // WiFi
-    WiFi.begin(); // // Connects to credential stored in the hardware.
-    //WiFi.begin("Your SSID", "Your password"); // or use it.
+    WiFi.begin("ocean", "oceanocean"); // or use it.
     WiFi.setSleep(false);
 
     int retry = 32;
-    M5.Display.setCursor(0,0);
     while(retry-- > 0 && WiFi.status() != WL_CONNECTED)
     {
-        M5.Display.printf(".");
+        ESP_LOGI("setup", ".");
         delay(200);
     }
     if(WiFi.status() != WL_CONNECTED)
     {
-        M5.Display.clear(TFT_RED);
-        M5.Display.setCursor(0,0);
-        M5_LOGE("Failed to connect WiFi");
+        ESP_LOGE("setup", "Failed to connect WiFi");
         delay(1000);
         abort();
     }
 
     // Server
     startCameraServer();
-    M5_LOGI("Camera ready use: http;//%s to connect", WiFi.localIP().toString().c_str());
+    ESP_LOGI("setup", "Camera ready use: http://%s to connect", WiFi.localIP().toString().c_str());
 
-    M5.Display.clear(TFT_DARKGREEN);
 }
 
 void loop()
